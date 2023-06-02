d62b26255f511fa2ee76d4d2d628f85f2169b73f
Frank Bernhardt <fra.bernhardt@googlemail.com>
2023-06-02 14:40:07 +0200

    fix

--- src/workflows/example_workflow.py
+++ src/workflows/example_workflow.py
@@ -9,27 +9,13 @@ name = "Example"
     image="ghcr.io/aiknow-public/hera-blueprint-base:main",
     image_pull_policy=ImagePullPolicy.always
 )
-def flip():
+def foo():
     import random
     from art import art
 
     # test external package
     art_1 = art("foo")
     print(art_1)
-
-    result = "heads" if random.randint(0, 1) == 0 else "tails"
-    print(result)
-
-
-@script()
-def heads():
-    print("it was the heads")
-
-
-@script()
-def tails():
-    print("it was tails")
-
 
 def workflow_content():
     """Returns the content of the desired workflow.
@@ -39,8 +25,5 @@ def workflow_content():
     :rtype: object
     """
     with DAG(name="d") as s:
-        f = flip()
-        heads().on_other_result(f, "heads")
-        tails().on_other_result(f, "tails")
-
+        f = foo()
 
