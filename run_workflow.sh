c439b2a997ae0e82d45b80aee3f5fc9ac44552ab
Frank Bernhardt <fra.bernhardt@googlemail.com>
2023-06-05 11:04:16 +0200

    time measurement

--- run_workflow.sh
+++ run_workflow.sh
@@ -12,15 +12,41 @@ if [ ! -f "./src/$1" ]; then
   exit 1
 fi
 
+# Check if measurement parameter is provided and set the measurement flag
+measurement_enabled=false
+if [ "$2" == "-m" ]; then
+  measurement_enabled=true
+fi
+
+# Function to start the execution timer
+start_timer() {
+  timer_start=$(date +%s.%N)
+}
+
+# Function to stop the execution timer and append result to CSV
+stop_timer() {
+  timer_end=$(date +%s.%N)
+  execution_time=$(echo "$timer_end - $timer_start" | bc)
+  if [ "$measurement_enabled" = true ]; then
+    echo "$1, $execution_time" >> execution_times.csv
+    echo "Execution time for $1: $execution_time seconds. Result appended to execution_times.csv."
+  fi
+}
+
 # build and push image
+start_timer
 docker login --username=$GH_WRITE_PACKAGE_USER --password=$GH_WRITE_PACKAGE ghcr.io
 docker buildx build --platform linux/amd64 -t ghcr.io/aiknow-public/hera-blueprint:$USER src/main
 docker push ghcr.io/aiknow-public/hera-blueprint:$USER
+stop_timer "Docker build and push"
 
 # run workflow
+start_timer
 export TASK_IMAGE="ghcr.io/aiknow-public/hera-blueprint:$USER"
 python "./src/$1"
-
 echo Wait for workflow to complete...
 argo wait @latest -n playground
+stop_timer "Workflow execution"
+
+# log workflow results
 argo logs @latest -n playground
--- src/main/samplemodule.py
+++ src/main/samplemodule.py
@@ -1,2 +1,2 @@
 def do_something():
-    print("bar7")
+    print("bar8")
