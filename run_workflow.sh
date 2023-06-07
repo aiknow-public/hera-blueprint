83a348654632ba580924f9e16d6104971811cde5
Frank Bernhardt <275327+frank-bee@users.noreply.github.com>
2023-06-07 09:09:01 +0000

    fix script to work on PRs

--- run_workflow.sh
+++ run_workflow.sh
@@ -1,25 +1,142 @@
 #!/bin/bash
 
+# Function to display the help message
+display_help() {
+  echo "Usage: ./script.sh [OPTIONS] FILENAME"
+  echo "Options:"
+  echo "  -p, --pr PR_NUMBER             Specify the pull request number"
+  echo "  -u, --user GITHUB_USER         Specify the GitHub username"
+  echo "  -r, --repository GITHUB_REPO   Specify the GitHub repository"
+  echo "  -d, --docker-user DOCKER_USER  Specify the Docker username"
+  echo "  -w, --docker-password DOCKER_PASSWORD"
+  echo "                                 Specify the Docker user password"
+  echo "  -m, --measure                  Enable measurement"
+  echo "  -h, --help                     Display this help message"
+}
+
+# Function to check if the Python script exists
+check_python_script() {
+  if [ ! -f "./src/$1" ]; then
+    echo "Python script does not exist."
+    exit 1
+  fi
+}
+
+# Function to get the pull request number created by the user
+get_pull_request() {
+  pr=$(gh pr list --base "main" --author "$user" --json number --state all --limit 1 | jq -r '.[].number')
+}
+
+# Set default values
+pr=""
+measurement_enabled=false
+user=""
+repository=""
+docker_user=""
+docker_user_password=""
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -p|--pr)
+      pr="$2"
+      shift 2
+      ;;
+    -u|--user)
+      user="$2"
+      shift 2
+      ;;
+    -r|--repository)
+      repository="$2"
+      shift 2
+      ;;
+    -d|--docker-user)
+      docker_user="$2"
+      shift 2
+      ;;
+    -w|--docker-password)
+      docker_user_password="$2"
+      shift 2
+      ;;
+    -m|--measure)
+      measurement_enabled=true
+      shift
+      ;;
+    -h|--help)
+      display_help
+      exit 0
+      ;;
+    *)
+      filename=$1
+      shift
+      ;;
+  esac
+done
+
 # Check if the Python script parameter is provided
-if [ -z "$1" ]; then
-  echo "Please provide filename of the Python workflow file."
+if [ -z "$filename" ]; then
+  echo "Please provide the filename of the Python workflow file."
   exit 1
 fi
 
 # Check if the Python script exists
-if [ ! -f "./src/$1" ]; then
-  echo "Python script does not exist."
-  exit 1
+check_python_script "$filename"
+
+# Set the 'user' variable if not provided
+if [ -z "$user" ]; then
+  if [ -n "$GITHUB_USER" ]; then
+    user=$GITHUB_USER
+  else
+    echo "GitHub username not provided."
+    exit 1
+  fi
 fi
 
-# Check if measurement parameter is provided and set the measurement flag
-measurement_enabled=false
-if [ "$2" == "-m" ]; then
-  measurement_enabled=true
+# Set the 'repository' variable if not provided
+if [ -z "$repository" ]; then
+  if [ -n "$GITHUB_REPOSITORY" ]; then
+    repository=$GITHUB_REPOSITORY
+  else
+    echo "GitHub repository not provided."
+    exit 1
+  fi
 fi
 
+# Set the 'docker_user' variable if not provided
+if [ -z "$docker_user" ]; then
+  if [ -n "$GH_WRITE_PACKAGE_USER" ]; then
+    docker_user=$GH_WRITE_PACKAGE_USER
+  else
+    echo "Docker username not provided."
+    exit 1
+  fi
+fi
 
-export TASK_IMAGE="ghcr.io/$GITHUB_REPOSITORY:$USER"
+# Set the 'docker_user_password' variable if not provided
+if [ -z "$docker_user_password" ]; then
+  if [ -n "$GH_WRITE_PACKAGE" ]; then
+    docker_user_password=$GH_WRITE_PACKAGE
+  else
+    echo "Docker user password not provided."
+    exit 1
+  fi
+fi
+
+# Check if PR parameter is provided, otherwise try to get existing pull request
+if [ -z "$pr" ]; then
+  get_pull_request
+  if [ -z "$pr" ]; then
+    echo "Please open a pull request first."
+    exit 1
+  fi
+fi
+
+# Run the Python script
+if [ "$measurement_enabled" = true ]; then
+  echo "Measurement enabled."
+fi
+
+export TASK_IMAGE="ghcr.io/$GITHUB_REPOSITORY:pr-$pr"
 
 # Function to start the execution timer
 start_timer() {
@@ -38,7 +155,7 @@ stop_timer() {
 
 # build and push image
 start_timer
-docker login --username=$GH_WRITE_PACKAGE_USER --password=$GH_WRITE_PACKAGE ghcr.io
+docker login --username=$docker_user --password=$docker_user_password ghcr.io
 docker buildx build --platform linux/amd64 -t $TASK_IMAGE src/main
 docker push $TASK_IMAGE
 stop_timer "Docker build and push"
