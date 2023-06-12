4a3b15a0137984270b73bf1e00aad0be992bf74d
Frank Bernhardt <fra.bernhardt@googlemail.com>
2023-06-12 13:37:05 +0200

    fix

--- run_workflow.sh
+++ run_workflow.sh
@@ -4,16 +4,18 @@
 display_help() {
   echo "Usage: ./script.sh [OPTIONS] FILENAME"
   echo "Options:"
-  echo "  -p, --pr PR_NUMBER             Specify the pull request number (required if not using codespaces, needed for the creation on an ephemeral branch"
-  echo "  -u, --user GITHUB_USER         Specify the GitHub username (required if not using codespaces to query for PRs)"
-  echo "  -r, --repository GITHUB_REPO   Specify the GitHub repository (required if not using codespaces, needed for the creation on an ephemeral branch)"
-  echo "  -dr, --docker-registry DOCKER_REGISTRY"
-  echo "                                 Specify the Docker registry (required if not using codespaces, needed for the creation on an ephemeral branch)"
-  echo "  -du, --docker-user DOCKER_USER  Specify the Docker username (required if not using codespaces, needed for the creation on an ephemeral branch)"
-  echo "  -dp, --docker-password DOCKER_PASSWORD"
-  echo "                                 Specify the Docker user password (required if not using codespaces, needed for the creation on an ephemeral branch)"
   echo "  -m, --measure                  Enable measurement"
   echo "  -h, --help                     Display this help message"
+  echo ""
+  echo "The following options are only needed if not working with codespaces but e.g. from local laptop:"
+  echo "  -p, --pr PR_NUMBER             Specify the pull request number"
+  echo "  -u, --user GITHUB_USER         Specify the GitHub username (required to query for PRs)"
+  echo "  -r, --repository GITHUB_REPO   Specify the GitHub repository (including org if github registry is used)"
+  echo "  -dr, --docker-registry DOCKER_REGISTRY"
+  echo "                                 Specify the Docker registry"
+  echo "  -du, --docker-user DOCKER_USER Specify the Docker username"
+  echo "  -dp, --docker-password DOCKER_PASSWORD"
+  echo "                                 Specify the Docker user password"
 }
 
 # Function to check if a command-line tool is installed
@@ -34,6 +36,17 @@ get_pull_request() {
   # Check if the required tools are installed
   check_tool_installed "gh"
   check_tool_installed "jq"
+
+  # Set the 'user' variable if not provided
+  if [ -z "$user" ]; then
+    if [ -n "$GITHUB_USER" ]; then
+      user=$GITHUB_USER
+    else
+      echo "GitHub username not provided."
+      exit 1
+    fi
+  fi
+
   pr=$(gh pr list --base "main" --author "$user" --json number --state all --limit 1 | jq -r '.[].number')
 }
 
@@ -100,24 +113,10 @@ fi
 # Check if the Python script exists
 check_python_script "$filename"
 
-# Set the 'user' variable if not provided
-if [ -z "$user" ]; then
-  if [ -n "$GITHUB_USER" ]; then
-    user=$GITHUB_USER
-  else
-    echo "GitHub username not provided."
-    exit 1
-  fi
-fi
-
-# Set the 'repository' variable if not provided
-if [ -z "$repository" ]; then
-  if [ -n "$GITHUB_REPOSITORY" ]; then
-    repository=$GITHUB_REPOSITORY
-  else
-    echo "GitHub repository not provided."
-    exit 1
-  fi
+# Check if ARGO_SERVER environment variable is set
+if [[ -z "$ARGO_SERVER" ]]; then
+  echo "ERROR: ARGO_SERVER environment variable is not set."
+  exit 1
 fi
 
 # Set the 'docker_registry' variable if not provided
@@ -130,22 +129,12 @@ if [ -z "$docker_registry" ]; then
   fi
 fi
 
-# Set the 'docker_user' variable if not provided
-if [ -z "$docker_user" ]; then
-  if [ -n "$DOCKER_USER" ]; then
-    docker_user=$DOCKER_USER
+# Set the 'repository' variable if not provided
+if [ -z "$repository" ]; then
+  if [ -n "$GITHUB_REPOSITORY" ]; then
+    repository=$GITHUB_REPOSITORY
   else
-    echo "Docker username not provided."
-    exit 1
-  fi
-fi
-
-# Set the 'docker_user_password' variable if not provided
-if [ -z "$docker_user_password" ]; then
-  if [ -n "$DOCKER_PASSWORD" ]; then
-    docker_user_password=$DOCKER_PASSWORD
-  else
-    echo "Docker user password not provided."
+    echo "GitHub repository not provided."
     exit 1
   fi
 fi
@@ -181,14 +170,49 @@ stop_timer() {
   fi
 }
 
+######
 # build and push image
+######
 start_timer
-docker login --username="$docker_user" --password="$docker_user_password" "$docker_registry"
+# Check if the user is already logged in to the Docker registry
+if ! docker info --format '{{.RegistryConfig.IndexConfigs}}' | grep -q "$docker_registry"; then
+  # Set the 'docker_user' variable if not provided
+  if [ -z "$docker_user" ]; then
+    if [ -n "$DOCKER_USER" ]; then
+      docker_user=$DOCKER_USER
+    else
+      echo "Docker username not provided."
+      exit 1
+    fi
+  fi
+
+  # Set the 'docker_user_password' variable if not provided
+  if [ -z "$docker_user_password" ]; then
+    if [ -n "$DOCKER_PASSWORD" ]; then
+      docker_user_password=$DOCKER_PASSWORD
+    else
+      echo "Docker user password not provided."
+      exit 1
+    fi
+  fi
+
+  docker login --username="$docker_user" --password="$docker_user_password" "$docker_registry"
+fi
+
 docker buildx build --platform linux/amd64 -t "$TASK_IMAGE" src/main
 docker push "$TASK_IMAGE"
+
+# Check the exit status of the docker push command
+push_status=$?
+if [[ $push_status -ne 0 ]]; then
+  echo "ERROR: Failed to push Docker image."
+  exit $push_status
+fi
 stop_timer "Docker build and push"
 
+#####
 # run workflow
+#####
 start_timer
 python "./src/$filename"
 echo "Wait for workflow to complete..."
